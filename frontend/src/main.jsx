83700f1c7b49e2f744a7e05a0fd300308b30ca65
ADITHYAKUMARREDDY <91198686+ADITHYAKUMARREDDY@users.noreply.github.com>
2025-11-10 03:34:45 +0530

    updated urls

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import axios from 'axios'
 
 // Configure axios base URL
-axios.defaults.baseURL = 'https://pulse-video-streaming-p.onrender.com'
+axios.defaults.baseURL =  import.meta.env.VITE_API_BASE || 'https://pulse-video-streaming-p.onrender.com'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
